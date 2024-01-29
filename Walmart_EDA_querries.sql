4e8e63d83589db4ae715579d70903689526c2e0b
tanveermarne20 <>
2024-01-29 14:33:52 +0530

    Csv loaded

--- Walmart_EDA_querries.sql
+++ Walmart_EDA_querries.sql
@@ -2,5 +2,23 @@
 ----------------Exploratory Data Analysis (EDA)----------------------
 --Generic Questions
 -- 1.How many distinct cities are present in the dataset?
+ SELECT DISTINCT CITY
+ FROM WALMARTUSA;
+
+SELECT *
+FROM WALMARTUSA;
+
+SELECT COUNT(*)
+FROM WALMARTUSA;
+
+SELECT DISTINCT COUNT(INVOICE_ID)
+FROM WALMARTUSA;
+
+SELECT COUNT(*)
+FROM WALMARTUSA;
+
+--HII IA M HERE
 
 
+SELECT *
+FROM WALMARTUSA;
