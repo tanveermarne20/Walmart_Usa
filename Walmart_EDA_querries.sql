c6d241dc2a022e8826b071e1939fd882d65532e3
tanveermarne20 <>
2024-01-30 18:09:07 +0530

    Questions added

--- Walmart_EDA_querries.sql
+++ Walmart_EDA_querries.sql
@@ -11,6 +11,45 @@
 
 --2.In which city is each branch situated?
 
+--3.How many Male customer are there?
+
+--4.List out all Male customer.
+
+--5.How many Female customer are there?
+
+
+--6.List out female customer
+
+
+
+--7.How many branches are there?
+
+
+
+--8.LIST OUT UNIQUE CUSTOMER_TYPE
+
+
+--9.List out unique city
+
+
+--10.Display details of customer wo have given rating more than 5
+
+
+--11.Display details of those customers who have made payment by cash
+
+
+--12.Display details of those customer who have purchased product in branch A
+
+
+
+--13.Display  customers purchased health and beatuty products
+
+
+
+--14.How many customers purchased health and beatuty products
+
+
+--15.How many customers belong to City yangon?
 
 
 
